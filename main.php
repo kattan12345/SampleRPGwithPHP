9bf111ce88d99ec39c7e0dc720e0219f366284b0
EC2 Default User <ec2-user@ip-172-31-24-175.us-east-2.compute.internal>
2021-01-02 15:19:56 +0000

    Print name of Human and Enemy instance

--- main.php
+++ main.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Hello world!" . PHP_EOL;
+require_once('./classes/Human.php');
+require_once('./classes/Enemy.php');
+
+$tiida = new Human();
+$goblin = new Enemy();
+
+$tiida->name = "ティーダ";
+$goblin->name = "ゴブリン";
+
+echo $tiida->name . PHP_EOL;
+echo $goblin->name . PHP_EOL;
