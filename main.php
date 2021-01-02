347bcf33f4390936a9bc764ad7ab54f115071218
EC2 Default User <ec2-user@ip-172-31-24-175.us-east-2.compute.internal>
2021-01-02 19:21:33 +0000

    Show Hitpoint

--- main.php
+++ main.php
@@ -11,3 +11,7 @@ $goblin->name = "ゴブリン";
 
 echo $tiida->name . PHP_EOL;
 echo $goblin->name . PHP_EOL;
+
+// 現在のHPの表示
+echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . PHP_EOL;
+echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . PHP_EOL;
