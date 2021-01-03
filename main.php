d9fce5819aa5659798aec2abf8aff7d5f070ece3
EC2 Default User <ec2-user@ip-172-31-24-175.us-east-2.compute.internal>
2021-01-03 10:59:03 +0000

    Add while loop

--- main.php
+++ main.php
@@ -12,17 +12,22 @@ $goblin->name = "ゴブリン";
 echo $tiida->name . PHP_EOL;
 echo $goblin->name . PHP_EOL;
 
-// 現在のHPの表示
+while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+    // 現在のHPの表示
+    echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . PHP_EOL;
+    echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . PHP_EOL;
+
+    echo PHP_EOL;
+
+    // 攻撃
+    $tiida->doAttack($goblin);
+    echo PHP_EOL;
+    $goblin->doAttack($tiida);
+    echo PHP_EOL;
+}
+
+echo "***Done***" . PHP_EOL;
 echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . PHP_EOL;
 echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . PHP_EOL;
 
-echo PHP_EOL;
 
-// 攻撃
-$tiida->doAttack($goblin);
-echo PHP_EOL;
-$goblin->doAttack($tiida);
-echo PHP_EOL;
-
-echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . PHP_EOL;
-echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . PHP_EOL;
