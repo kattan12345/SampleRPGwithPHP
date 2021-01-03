9e3a4dbd0f8899b382cc5249170be342ab89790d
EC2 Default User <ec2-user@ip-172-31-24-175.us-east-2.compute.internal>
2021-01-03 11:34:21 +0000

    Override doAttack function in Brave class

--- classes/Brave.php
+++ classes/Brave.php
@@ -1,5 +1,14 @@
 <?php
 
 class Brave extends Human {
-    
+    public function doAttack($enemy) {
+        if (rand(1, 3) === 1) {
+            echo "『" . $this->name . "』のスキル発動" . PHP_EOL;
+            echo "『全力斬り』" . PHP_EOL;
+            echo $enemy->name . "に" . $this->attackPoint * 1.5 . "のダメージ" . PHP_EOL;
+            $enemy->tookDamage($this->attackPoint * 1.5);
+        } else {
+            parent::doAttack($enemy);
+        }
+    }
 }
