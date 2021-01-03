8d4facf7601d3027a492096d4b1d14ada03a13ea
EC2 Default User <ec2-user@ip-172-31-24-175.us-east-2.compute.internal>
2021-01-03 11:38:45 +0000

    Override properties in Brave class

--- classes/Brave.php
+++ classes/Brave.php
@@ -1,6 +1,11 @@
 <?php
 
 class Brave extends Human {
+    
+    const MAX_HITPOINT = 120;
+    public $hitPoint = self::MAX_HITPOINT;
+    public $attackPoint = 30;
+    
     public function doAttack($enemy) {
         if (rand(1, 3) === 1) {
             echo "『" . $this->name . "』のスキル発動" . PHP_EOL;
@@ -11,4 +16,5 @@ class Brave extends Human {
             parent::doAttack($enemy);
         }
     }
+    
 }
